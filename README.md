b900dbed98d56f3daf163a024360c8a3711f4446
Student <example@example.com>
2019-08-22 16:28:27 -0700

    Add join, splice and include methods

---
+++ README.md
@@ -0,0 +1,9 @@
+here are the rules of Pig Latin:
+
+For words beginning with a vowel, add "way" to the end.
+
+For words beginning with one or more consonants, move all of the first consecutive consonants to the end, and add "ay".
+
+If the first consonants include "qu", move the "u" along with the "q". Don't forget about words like "squeal" where "qu" doesn't come first!
+
+For words beginning with "y", treat "y" as a consonant
--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
   $("form#formword").submit(function(event) {
     event.preventDefault();
- var inputString = $("#input").val();
- var inputArray = inputString.split("");
- var vowel = ["a","e","i","o","u"];
- var output = "";
- for(var i = 0; i<vowel.length; i++) {
-    if(inputArray[0] === vowel[i]) {
-        output = inputString + "way";
-     }
-   }
-   var outputArray = inputArray.slice(0);
-   for(var i = 0; i<inputArray.length; i++) {
-     var isVowel = false;
-     for(var j = 0; j<vowel.length; j++){
-       if(inputArray[i] === vowel[j]) {
-         isVowel = true;
-        }
+    var inputString = $("#input").val();
+    var inputArray = inputString.split("");
+    var vowel = ["a","e","i","o","u"];
+    var output = "";
+
+    if (vowel.includes(inputArray[0])){
+      output = inputString + "way";
+    } else if(inputArray[0] === "q"  &&  inputArray[1] === 'u'){
+      var qu = inputArray.splice(0,2);
+      output = inputArray.join("") + qu.join("") + "ay";
+    } else {
+      var consonants = "";
+      for(var i=0; i<inputArray.length; i++){
+        // if current letter is a vowel
+        // checks if the current letter, which is inputArray[i], is included in the vowel array.
+        if(!vowel.includes(inputArray[i])) {
+        consonants += inputArray.shift();
       }
-      if (!isVowel) {
-        var consonant = outputArray.shift();
-        outputArray.push(consonant);
-      }else {
-        break;
+      output = inputArray.join("") + consonants + "ay";
+    } //if(inputArray[0] === "y"){
+      //var consonants = "";
+      //for(var i=0; i<inputArray.length; i++){
+      //  if(!vowel.includes(inputArray[i]))
+      //  consonants += inputArray.shift();
       }
-   }
-    alert(outputArray);
+      //output = inputArray.join("") + consonants + "ay";
+    
 
 
 
@@ -33,6 +35,8 @@ $(document).ready(function() {
 
 
 
-  $("#result").text(output);
+
+
+    $("#result").text(output);
   });
 });
